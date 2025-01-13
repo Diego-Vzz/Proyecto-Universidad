e77d1bec6e9e9cc922c7b3a850adb18929325587
programador3 <gama.programador3@outlook.com>
2025-01-13 16:31:03 -0600

    Home, router, style
    >> Creacion de componente Home.
    
    router.ts
    >> Se agrego ruta de Home.
    
    style.css
    >> Se agrego estilos predeterminados.

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 import PruebaView from './views/PruebaView.vue';
+import Home from './views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
     { path: '/Prueba', name: 'prueba', component: PruebaView, meta: { layout: "Main" } },
+    { path: '/', name: 'home', component: Home, meta: { layout: "Main" } },
 ];
 
 const router = createRouter({
