cf3dc3bde723ee26cb81b3c944091999c335847e
programador3 <gama.programador3@outlook.com>
2025-01-21 13:34:22 -0600

    Cambio de backend

--- backend/config/corsOptions.js
+++
@@ -1,7 +0,0 @@
-const corsOptions = {
-    origin: process.env.CORS_ORIGIN || "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-};
-
-module.exports = corsOptions;
--- backend/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.status || 599).json({
-        error: true,
-        message: err.message || "Error interno del servidor",
-    });
-};
-
-module.exports = { errorHandler };
